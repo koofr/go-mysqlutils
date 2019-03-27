31e1563e01325c7bf451802d5ce0a428249449be
Luka Zakrajšek <luka@bancek.net>
2019-03-27 19:08:49 +0100

    Add ctx to Retry

--- retry.go
+++ retry.go
@@ -1,6 +1,7 @@
 package mysqlutils
 
 import (
+	"context"
 	"database/sql"
 	"errors"
 	"math/rand"
@@ -8,12 +9,12 @@ import (
 )
 
 type DB interface {
-	Begin() (*sql.Tx, error)
+	BeginTx(ctx context.Context, opts *sql.TxOptions) (*sql.Tx, error)
 }
 
 var ErrForceRetry = errors.New("mysqlutils force retry")
 
-func Retry(db DB, retries int, expectedErrorNumbers ...int) func(f func(*sql.Tx) error) error {
+func Retry(ctx context.Context, db DB, retries int, expectedErrorNumbers ...int) func(f func(*sql.Tx) error) error {
 	return func(f func(*sql.Tx) error) error {
 		var lastErr error
 
@@ -23,7 +24,7 @@ func Retry(db DB, retries int, expectedErrorNumbers ...int) func(f func(*sql.Tx)
 				time.Sleep(time.Duration(i*rand.Intn(50)) * time.Millisecond)
 			}
 
-			tx, err := db.Begin()
+			tx, err := db.BeginTx(ctx, nil)
 			if err != nil {
 				if IsErrorIn(err, expectedErrorNumbers...) {
 					lastErr = err
@@ -60,6 +61,6 @@ func Retry(db DB, retries int, expectedErrorNumbers ...int) func(f func(*sql.Tx)
 	}
 }
 
-func RetryDefault(db *sql.DB, retries int) func(f func(*sql.Tx) error) error {
-	return Retry(db, retries, ERROR_LOCK_DEADLOCK, ERROR_QUERY_INTERRUPTED, ERROR_LOCK_WAIT_TIMEOUT)
+func RetryDefault(ctx context.Context, db *sql.DB, retries int) func(f func(*sql.Tx) error) error {
+	return Retry(ctx, db, retries, ERROR_LOCK_DEADLOCK, ERROR_QUERY_INTERRUPTED, ERROR_LOCK_WAIT_TIMEOUT)
 }
