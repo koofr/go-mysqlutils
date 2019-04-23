117750b66295415e5f629081e5de4d93b812b4cf
Luka Zakrajšek <luka@bancek.net>
2019-04-23 22:47:52 +0200

    Add ERROR_NO_SUCH_TABLE

--- errors.go
+++ errors.go
@@ -9,6 +9,7 @@ const (
 	ERROR_LOCK_WAIT_TIMEOUT = 1205
 	ERROR_LOCK_DEADLOCK     = 1213
 	ERROR_QUERY_INTERRUPTED = 1317
+	ERROR_NO_SUCH_TABLE     = 1146
 )
 
 func IsErrorIn(err error, expectedErrorNumbers ...int) bool {
